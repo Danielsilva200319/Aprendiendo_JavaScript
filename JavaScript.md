af56eb1ecdaa5f728419dab3dc4ce89ee92334ed
Daniel silva <89894235+Danielsilva200319@users.noreply.github.com>
2023-09-26 07:48:42 -0500

    Add files via upload

---
+++ JavaScript.md
@@ -0,0 +1,3 @@
+documente.write() = sirve para que me muestre el valor de una variable en el html, sin tener que ser un alert.
+
+alert() = sirve para mostrar variables pero como una alerta.
---
+++ main.js
@@ -0,0 +1,10 @@
+// let numero = 10;
+// let numero1 = 5;
+// resultado = -numero;
+// document.write(resultado);
+// alert(resultado);
+
+/* CONCATENACIÓN */
+nombre = "Lucas Dalto";
+frase = `Soy ${nombre} y estoy caminando`;
+document.write(frase);
